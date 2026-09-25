8992792faf9c1f8c3e894f08ee836d728b869f61
Alan Patrizio Lazaro Herrera <patrizio1432004@gmail.com>
2026-09-24 21:38:02 -0600

    Add files via upload

---
+++ Pruebas/Mdulo GPS 1/prueba_gps.ino
@@ -0,0 +1,18 @@
+#include <Adafruit_TinyUSB.h>
+
+void setup() {
+
+  Serial.begin(115200);
+  while (!Serial) { delay(10); }
+
+  Serial.println("Iniciando prueba del módulo GPS SIM28...");
+
+  Serial1.begin(9600);
+}
+
+void loop() {
+
+  while (Serial1.available()) {
+    Serial.write(Serial1.read());
+  }
+}
---
+++ Pruebas/Mdulo GPS 1/tiny_gps_plus.ino
@@ -0,0 +1,35 @@
+#include <Adafruit_TinyUSB.h> 
+#include <TinyGPS++.h>
+
+TinyGPSPlus gps;
+unsigned long ultimoMensaje = 0;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) { delay(10); }
+
+  Serial.println("Iniciando decodificador GPS...");
+  
+  Serial1.begin(9600); 
+}
+
+void loop() {
+  while (Serial1.available() > 0) {
+    gps.encode(Serial1.read());
+  }
+
+  if (millis() - ultimoMensaje > 2000) {
+    
+    if (gps.location.isValid()) {
+      Serial.print("Latitud: ");
+      Serial.print(gps.location.lat(), 6); // 6 decimales de precisión
+      Serial.print(" | Longitud: ");
+      Serial.println(gps.location.lng(), 6);
+    } 
+    else {
+      Serial.println("Buscando satélites... (Lleva el módulo al exterior o a una ventana)");
+    }
+    
+    ultimoMensaje = millis();
+  }
+}
